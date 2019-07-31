30f89b6ac3c226c67d7650a5689e73a715fa31f1
Manuela Gomez <mmanu.gomez@gmail.com>
2019-07-30 23:49:26 -0400

    Complete functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,33 @@
+//remember First in, First Out - Enter at the end and leave from the front
 var Queue = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
+  // Some thoughts: can we use closure to keep track of numeric keys - last key?
   var storage = {};
-
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    //Remember: add at the end
+    var size = someInstance.size();
+    var nextKey = (size>0) ? size++: 0;
+    storage[nextKey] = value;
   };
 
   someInstance.dequeue = function() {
+    //save the value being deleted
+    dequeueValue = storage[0];
+    //we can delete the first key
+    delete storage[0];
+    var keys = Object.keys(storage);
+    //go over each key and decrease by 1
+    for (var i=0; i<keys.length;i++){
+      storage[Number(keys[i])-1]=storage[keys[i]]; //assign to previous key -> move one step in line
+      delete storage[keys[i]]; //delete that previous key
+    }
+    return dequeueValue;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,20 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var size = someInstance.size();
+    var nextKey = (size>0) ? size++: 0;
+    storage[nextKey] = value;
   };
 
   someInstance.pop = function() {
+    var size = someInstance.size()-1;
+    let poppedValue = storage[size];
+    delete storage[size];
+    return poppedValue;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
