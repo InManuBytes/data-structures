5f7ddb70a36e585b7f507cded21fbb1d6f33c486
Manuela Gomez <mmanu.gomez@gmail.com>
2019-07-31 23:00:40 -0400

    Added functional-shared skeleton for both queue and stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  /**
+   * Remember don't write the word t h i s, not even inside the comments as one of the tests
+   * will catch reference in es6
+   */
+
+  // Declaring the variable below means that we do not extend the constructor function
+  // i.e. that the Object.keys(Queue).length = 0
+  var someQueueInstance = {};
+  someQueueInstance.enqueue = queueMethods.enqueue;
+  someQueueInstance.dequeue = queueMethods.dequeue;
+  someQueueInstance.size = queueMethods.size;
+  return someQueueInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  storage: {},
+  enqueue: function(){
 
+  },
+  dequeue: function(){
 
+  },
+  size: function(){
+    return Object.keys(queueMethods.storage).length;
+  }
+};
+
+var queue = new Queue();
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someQueueInstance = {};
+  someQueueInstance.push = queueMethods.push;
+  someQueueInstance.pop = queueMethods.pop;
+  someQueueInstance.size = queueMethods.size;
+  return someQueueInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  storage: {},
+  enqueue: function(){
+
+  },
+  dequeue: function(){
+
+  },
+  size: function(){
+    return Object.keys(queueMethods.storage).length;
+  }
+};
 
 
