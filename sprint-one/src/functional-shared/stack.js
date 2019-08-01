5bdc3529fc549471aeebb621f6eff94785b3c9ad
Manuela Gomez <mmanu.gomez@gmail.com>
2019-07-31 23:31:05 -0400

    Passing all but 2 tests stack shared behavior

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -3,22 +3,26 @@ var Stack = function() {
   // Declaring the variable below means that we do not extend the constructor function
   //i.e. that the Object.keys(Queue).length = 0
   var someQueueInstance = {};
-  someQueueInstance.push = queueMethods.push;
-  someQueueInstance.pop = queueMethods.pop;
-  someQueueInstance.size = queueMethods.size;
+  someQueueInstance.push = stackMethods.push;
+  someQueueInstance.pop = stackMethods.pop;
+  someQueueInstance.size = stackMethods.size;
   return someQueueInstance;
 };
 
 var stackMethods = {
+  //this feels problematic but not sure where else to put it
   storage: {},
-  push: function(){
-
+  push: function(value){
+    var nextKey = stackMethods.size()+1;
+    stackMethods.storage[nextKey] = value;
   },
   pop: function(){
-
+    var lastIn = stackMethods.storage[stackMethods.size()];
+    delete stackMethods.storage[stackMethods.size()];
+    return lastIn;
   },
   size: function(){
-    return Object.keys(queueMethods.storage).length;
+    return Object.keys(stackMethods.storage).length;
   }
 };
 
