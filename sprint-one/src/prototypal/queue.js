8743fa8e2a6f7a56d9e7a6022088e5768fa73970
Manuela Gomez <mmanu.gomez@gmail.com>
2019-08-01 22:34:24 -0400

    Wrote out enqueue function

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -7,15 +7,21 @@ var Queue = function() {
 
 var queueMethods = {
   storage: {},
+  //we'll keep track of who's at the front and who's at the back [numerical key]
+  //since those are the two that matter
+  //this will help us get the size of the queue
   front: 0,
   back: 0,
-  enqueue: function(){
-
+  enqueue: function(value){
+    var nextKey = queueMethods.back;
+    queueMethods.back++;
+    queueMethods.storage[nextKey] = value;
   },
   dequeue: function(){
     //if there's nothing in the queue, might as well not do anything
-    if (queueMethods.front > 0){
-
+    if (queueMethods.size > 0){
+      var lastIn
+      return lastIn;
     }
   },
   size: function(){
