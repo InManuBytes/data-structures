71e6e00b3e9195406a0142814d32b8da14204276
Manuela Gomez <mmanu.gomez@gmail.com>
2019-08-03 18:30:19 -0400

    Set up the functional-shared o Please enter the commit message for your changes. Lines starting

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,16 +1,25 @@
 var Tree = function(value) {
   var newTree = {};
-  newTree.value = value;
-
+  newTree.value = value; //first value = undefined? if
   // your code here
+  // functional-shared style
+  extend(newTree,treeMethods)
   newTree.children = null;  // fix me
 
   return newTree;
 };
 
+var extend = function(destination,newObject) {
+  for (var key in newObject) {
+    destination[key] = newObject[key];
+  }
+}
+
+
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+
 };
 
 treeMethods.contains = function(target) {
