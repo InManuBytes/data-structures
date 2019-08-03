a2a3714a2d1677635270e9040a343b513742fded
Manuela Gomez <mmanu.gomez@gmail.com>
2019-08-02 21:04:58 -0400

    Complete pseudoclassical stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,17 +1,25 @@
-var Queue = function() {
+var Queue = function () {
   this.storage = {};
   this.front = 0;
   this.back = 0;
 };
 
-Queue.prototype.enqueue = function (){
-
-}
+Queue.prototype.enqueue = function (value) {
+  var nextKey = this.back;
+  this.back++;
+  this.storage[nextKey] = value;
+};
 
 Queue.prototype.dequeue = function () {
-
-}
+  if ( this.size() > 0 ) {
+    var firstKey = this.front;
+    var firstIn = this.storage[firstKey];
+    delete this.storage[firstKey];
+    this.front++;
+    return firstIn;
+  }
+};
 
 Queue.prototype.size = function () {
-
-}
+  return this.back - this.front;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,5 +1,5 @@
 var Stack = function() {
-  this.storage ={};
+  this.storage = {};
   this.top = 0;
 };
 
@@ -7,18 +7,18 @@ Stack.prototype.push = function (value) {
   var nextKey = this.top;
   this.top++;
   this.storage[nextKey] = value;
-}
+};
 
 Stack.prototype.pop = function () {
-  if (this.size() > 0){
+  if ( this.size() > 0 ) {
     this.top--;
     var lastKey = this.top;
     var lastIn = this.storage[lastKey];
     delete this.storage[lastKey];
     return lastIn;
   }
-}
+};
 
-Stack.prototype.size = function (){
+Stack.prototype.size = function () {
   return this.top;
-}
+};
