3da8d8f3ab8e0d18b6208537cc2535effd1da493
Manuela Gomez <mmanu.gomez@gmail.com>
2019-08-03 18:58:52 -0400

    Wrote out addChild function for trees

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,10 +1,11 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value; //first value = undefined? if
+  debugger
   // your code here
   // functional-shared style
   extend(newTree,treeMethods)
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me => set to null originally
 
   return newTree;
 };
@@ -15,10 +16,41 @@ var extend = function(destination,newObject) {
   }
 }
 
+//example
+/**
+ * Tree().addChild(5)
+ * Tree().addChild(6)
+ * Tree().children[0].addChild(7)
+ *
+ *
+ */
+// tree = {
+//   value: undefined, -> can call it parent?
+//   children: [
+//         {value: 5,
+//          children: [
+//            {value: 7,
+//             children: []
+//            }
+//          ]
+//         },
+//         {value: 6,
+//          children: []
+//         }
+//   ]
+// }
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  debugger
+  // newTree.children -> array
+  // make a tree?
+  // set newTree.value to input value
+  var child = Tree(value) //where to invoke? - replicating a tree = subtree
+  this.children.push(child)
+  // push to the parent
+
 
 };
 
