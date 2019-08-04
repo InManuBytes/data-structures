a07dba1516bbf3d1878f50106a37fc8f948e90f5
Manuela Gomez <mmanu.gomez@gmail.com>
2019-08-04 14:01:42 -0400

    Cleaned up comments

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,12 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value; //first value = undefined? if
-  debugger
-  // your code here
   // functional-shared style
   extend(newTree,treeMethods)
   newTree.children = [];  // fix me => set to null originally
-
   return newTree;
 };
 
@@ -19,7 +16,6 @@ var extend = function(destination,newObject) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  debugger
   // newTree.children -> array
   // make a tree?
   // set newTree.value to input value
@@ -64,15 +60,12 @@ treeMethods.contains = function(target) {
         result = result || this.children[i].contains(target); //how can we nest into the children?
       }
     }
-  return (result);
+  return result;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
  // extend  - linear O(n) -> in our case it's constant for every instance of a tree
  // addChild - you're pushing to an array => constant because you're inserting at the end O(1)
  // contains - polynomial depends on the height of the tree? - O(n^k) - k if a node has a child [revisit?]
