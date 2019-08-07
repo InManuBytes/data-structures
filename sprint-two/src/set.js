fa7bc11c4536f26ad81a6e68336b39ed35faa63b
Manuela Gomez <mmanu.gomez@gmail.com>
2019-08-06 22:39:44 -0400

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // will I be an object? or an array?
   return set;
 };
 
 var setPrototype = {};
-
+// items => strings
 setPrototype.add = function(item) {
+  //add item to storage
+  if(!this.contains(item)) {
+    this._storage.push(item);
+  }
+  //let's not add things twice
 };
 
 setPrototype.contains = function(item) {
+  //loop through the storage array and find matching item
+  return this._storage.includes(item);
+
 };
 
 setPrototype.remove = function(item) {
+  if (this.contains(item)) {
+    var itemIdx = this._storage.indexOf(item)
+    this._storage.splice(itemIdx,1);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+// O(N^2)
